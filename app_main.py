ad8b574ee6d21fa75c763c6aa8a24021ced11cab
urvv1y <urvalek.lukas21@gmail.com>
2026-07-26 15:01:19 +0200

    new functionality

--- app_main.py
+++ app_main.py
@@ -5,3 +5,12 @@ user_variable_count: int = input("Enter the number of variables (1-26): ")
 table = main.fill_the_table(int(user_variable_count))
 main.print_table(table)
 
+new_table =main.expand_table_by_new_variable(table, "D")
+main.print_table(new_table)
+
+expanded = main.expand_table_by_logical_formula(new_table, "A and B")
+main.print_table(expanded)
+
+expanded = main.expand_table_by_logical_formula(expanded, "C or D")
+main.print_table(expanded)
+
--- main_logic.py
+++ main_logic.py
@@ -41,4 +41,34 @@ def print_table(table: list[list[str]]):
         print(" | ".join(row))
 
 
+def expand_table_by_new_variable(table: list[list[str]], new_variable_letter: str) -> list[list[str]]:
+    """Expand the existing truth table by adding a new variable column."""
+    
+    current_var_count = len(table[0])
+    next_var_letter = ALPHABET[current_var_count]
+    
+    new_header = table[0] + [new_variable_letter]
+    new_table = [new_header]
+    
+    data_rows = table[1:]
+    
+    for row in data_rows:
+        new_table.append(row + ["0"])
+        
+    for row in data_rows:
+        new_table.append(row + ["1"])
+        
+    return new_table
 
+def expand_table_by_logical_formula(table: list[list[str]], formula: str) -> list[list[str]]:
+    """Add a new column for a logical formula and leave the values empty for now."""
+    if not table:
+        return [[formula]]
+
+    new_header = table[0] + [formula]
+    new_table = [new_header]
+
+    for row in table[1:]:
+        new_table.append(row + [""])
+
+    return new_table
